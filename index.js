0194aa6f1c841cfd78a1f98d74e5007cfce017f8
Jacob <weaver.jacob.t@gmail.com>
2023-09-23 19:32:09 -0500

    this

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+
+/* 1. [How can we "iterate" through individual words in a string?]
+
+const str = 'i have learned something new today';
+              //---------split the above string into an array of strings 
+              //---------whenever a blank space is encountered
+const arr = str.split(" ");
+              //---------loop through each element of the array and capitalize the first letter.
+for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+}
+              //---------Join all the elements of the array back into a string 
+              //---------using a blankspace as a separator 
+const str2 = arr.join(" ");
+
+*/
+
+/* 2. [Can we execute an iteration inside an iteration? How?]
+
+*/
+
+/* 3. [How can we capitalize just the first letter in a word?]
+
+const word = "freecodecamp"
+const capitalized =
+  word.charAt(0).toUpperCase()
+  + word.slice(1)
+
+  */
+
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +44,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(function (word) {
+      
+      const arr = word.split(" ");
+      for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+      }
+      const word2 = arr.join(" ");
+    
+      return word2;
+      });
+
+
+ 
 }
